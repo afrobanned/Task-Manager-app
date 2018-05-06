07f7ed68b0b09c978b40a6ac0a2c13b464d678d0
Christopher Reepen <36974955+afrobanned@users.noreply.github.com>
2018-05-06 14:09:15 -0700

    lot of work

--- scripts.js
+++ scripts.js
@@ -5,21 +5,5 @@ $.getJSON( "https://api.airtable.com/v0/appUAJSEFDMl7iv8C/Tasks?api_key=keyQ7f5Y
     items.push( "<li id='" + val.id + "'>" + val.fields["Tasks"] + '<input type="checkbox">'+ "</li>" );
   });
 
-  $( "<ul/>", {
-    "class": "my-new-list",
-    html: items.join( "" )
-  }).appendTo( "body" );
+$(".task-list" ).append(items.join(""));
 });
-function openWeekday(evt, weekdayWhich) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(weekdayWhich).style.display = "block";
-    evt.currentTarget.className += " active";
-}
